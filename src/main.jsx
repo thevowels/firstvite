0dda60ba8407c47a417036f289f80f32944ad36c
thevowels <aungphyohtet1@gmail.com>
2024-12-27 05:08:13 +0700

    adding react-router-dom doing it on my own way. I thought I could use approuter feature but somehow, i can't right now may be due to vite's build or that works only on next

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import './App.css'
+
+// import App from './App.jsx'
+import Home from "./components/home.jsx";
+import Abc from "./abc/page.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="abc" element={<Abc />}/>
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
